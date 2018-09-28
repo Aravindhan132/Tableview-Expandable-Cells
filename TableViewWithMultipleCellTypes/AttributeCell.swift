90dd29d04bd0bd6c1a92a49b41f1a41edf781378
Aravindhan132 <virataravindhan@gmail.com>
2018-09-28 12:58:47 +0530

    source code

---
+++ TableViewWithMultipleCellTypes/AttributeCell.swift
@@ -0,0 +1,24 @@
+ 
+
+import UIKit
+
+class AttributeCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var valueLabel: UILabel?
+    
+    var item: Attribute?  {
+        didSet {
+            titleLabel?.text = item?.key
+            valueLabel?.text = item?.value
+        }
+    }
+    
+    static var nib:UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
---
+++ TableViewWithMultipleCellTypes/EmailCell.swift
@@ -0,0 +1,26 @@
+ 
+
+import UIKit
+
+class EmailCell: UITableViewCell {
+
+    @IBOutlet weak var emailLabel: UILabel?
+    
+    var item: ProfileViewModelItem? {
+        didSet {
+            guard let item = item as? ProfileViewModelEmailItem else {
+                return
+            }
+            
+            emailLabel?.text = item.email
+        }
+    }
+
+    static var nib:UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
